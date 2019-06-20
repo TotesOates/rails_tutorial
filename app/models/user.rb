2d89237de18e37c2051e86d1724fdc9dac1b7653
Joe Chen <joseph.chen.od@gmail.com>
2019-06-19 21:49:29 -0500

    remove comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-    # id
-    # name
-    # email
-    has_many :microposts
-    validates :name, presence: true    # Replace FILL_IN with the right code.
-    validates :email, presence: true    # Replace FILL_IN with the right code.
+  # id
+  # name
+  # email
+  has_many :microposts
+  validates :name, presence: true
+  validates :email, presence: true
 end
