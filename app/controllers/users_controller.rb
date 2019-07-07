82b56d5f9fd8f198726bc44e1deff3a06f6a2a23
Joe Chen <joseph.chen.od@gmail.com>
2019-07-06 20:00:37 -0500

    tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,15 +27,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    # respond_to do |format|
     if @user.save
-      # format.html { redirect_to @user, notice: "User was successfully created." }
-      # format.json { render :show, status: :created, location: @user }
+      log_in(@user)
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
-      # format.html { render :new }
-      # format.json { render json: @user.errors, status: :unprocessable_entity }
       flash[:danger] = "Sorry, something messed up."
       render "new"
     end
