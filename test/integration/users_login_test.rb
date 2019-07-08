9c92f74cbedfb6779577164f61c46f5f9417eb68
Joe Chen <joseph.chen.od@gmail.com>
2019-07-07 21:32:32 -0500

    finish chap 8

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -26,4 +26,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
   end
+
+  test "log in and then log out" do
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert logged_in?
+    delete logout_path
+    assert_not logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+  end
 end
