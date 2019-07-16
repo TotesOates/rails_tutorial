937bd4ef420254f75853a8922b77fbe0c0b05a61
Joe Chen <joseph.chen.od@gmail.com>
2019-07-15 21:44:45 -0500

    fix 10.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,6 @@ class UsersController < ApplicationController
   end
 
   # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
 
@@ -36,27 +35,22 @@ class UsersController < ApplicationController
       flash[:danger] = "Sorry, something messed up."
       render "new"
     end
-    # end
   end
 
   # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        # format.html { redirect_to @user, notice: "User was successfully updated." }
-        # format.json { render :show, status: :ok, location: @user }
-        flash[:success] = 'Profile Updated'
-        redirect_to @user
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    set_user
+
+    if set_user.update_attributes(user_params) 
+      flash[:success] = 'Profile Updated'
+      redirect_to @user
+    else
+      flash[:danger] = 'Profile Update Failed'
+      render 'edit'
     end
   end
 
   # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.destroy
     respond_to do |format|
